730e0924a773c5702a7b694c7b170581690e35ef
Charles Burt <ctf_exe@hotmail.com>
2023-02-18 19:24:34 +0000

    removed commented out code

--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -38,34 +38,3 @@ int termFound = findNumber(list, 0, list.Length, searchTerm);
 long endTime = System.DateTime.Now.Millisecond;
 
 Console.WriteLine("Term found = " + termFound + "\nTime taken = " + (endTime - startTime) + " m.\nFunction calls = " + count);
-
-/*int count = 0;
-int findNumber(int[] collection, int start, int end, int x)
-{
-    count++;
-    if(end >= start)
-    {
-        int middle = start + (end - start) / 2;
-        if (collection[middle] == x)
-            return middle;
-        if(collection[middle] > x)
-            return findNumber(collection, start, middle - 1, x);
-        return findNumber(collection, middle + 1, end, x);
-    }
-    return -1;
-}
-
-
-int[] numberCollection = new int[1000000];
-for (int i = 0; i < numberCollection.Length; i++)
-{
-    numberCollection[i] = i;
-}
-
-long startTime = System.DateTime.Now.Millisecond;
-
-findNumber(numberCollection, 0, numberCollection.Length, 999999);
-
-Console.WriteLine("We found the value at " + count + " try");
-long endTime = System.DateTime.Now.Millisecond;
-Console.WriteLine("Total time is " + (endTime-startTime).ToString() + " ms");*/
