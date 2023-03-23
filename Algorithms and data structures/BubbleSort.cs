91b9bae2477e71ccd60c239f9c5bb493927a3a24
charlesburt01 <ctf_exe@hotmail.com>
2023-03-23 07:53:40 +0000

    added bubble sort

---
+++ Algorithms and data structures/BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿//Bubble Sort Code Implementation
+int[] ListToSort = new int[] { 4, 2, 7, 1, 3 };
+
+int[] BubbleSort(int[] list)
+{
+    bool sorted = false;
+    int unsortedUntil = list.Length - 1;
+
+    while (!sorted)
+    {
+        sorted = true;
+        for (int i = 0; i < unsortedUntil; i++)
+        {
+            if (list[i] > list[i + 1])
+            {
+                int n = list[i];
+                int n1 = list[i + 1];
+                list[i] = n1;
+                list[i + 1] = n;
+                sorted = false;
+            }
+        }
+        unsortedUntil--;
+    }
+
+    return list;
+}
+
+int[] sortedList = BubbleSort(ListToSort);
+string list = "";
+for (int i = 0; i < sortedList.Length; i++)
+{
+    list += sortedList[i].ToString();
+}
+Console.WriteLine(list);
--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -47,57 +47,5 @@ no example yet
 
 */
 
-/*
-int count = 0;
 
-int fibonacci(int number)
-{
-    count++;
-    if (number <= 1) return number;
 
-    return fibonacci(number - 2) + fibonacci(number - 1);
-}
-
-int input = 6;
-int output = fibonacci(input); 
-Console.WriteLine("Input = {0}, Output = {1}, done {2} operations", input, output, count);
-*/
-
-/*
-HashSet<int> set = new HashSet<int>();
-set.Add(1);
-set.Add(2);
-set.Add(3);
-set.Add(3);
-set.Add(1);
-
-foreach (int element in set)
-{
-    Console.WriteLine(element);
-}*/
-
-/*int binary_search(int[] array, int value)
-{
-    Array.Sort(array);
-    int lowerBound = 0;
-    int upperBound = array.Length -1;
-
-    int count = 0;
-    while (lowerBound <= upperBound)
-    {
-        Console.WriteLine("Loop {0}", count);
-        count++;
-        int middleIndex = (upperBound + lowerBound) / 2;
-        int valueAtMiddleIndex = array[middleIndex];
-
-        if (value < valueAtMiddleIndex)
-            upperBound = middleIndex - 1;
-        else if (value > valueAtMiddleIndex)
-            lowerBound = middleIndex + 1;
-        else if (value == valueAtMiddleIndex)
-            return valueAtMiddleIndex;
-    }
-    return -1;
-}
-
-Console.WriteLine(binary_search(new int[4] { 17, 3, 202, 80 }, 3));*/
