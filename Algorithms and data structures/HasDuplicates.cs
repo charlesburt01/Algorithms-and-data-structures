c9e6c6d8ba53e330732137fc8289d853ce90709e
charlesburt01 <ctf_exe@hotmail.com>
2023-03-24 18:22:00 +0000

    adding selection sort and has duplicates

--- Algorithms and data structures/HasDuplicates.cs
+++ Algorithms and data structures/HasDuplicates.cs
@@ -1,7 +1,7 @@
 ﻿
 
 int steps = 0;
-//Slow example of a function of N^2 as it uses nested loops, there is a better way to write this using python, and arrays that don't require you to set a size, whereas C# requires you to do this. Use a list I hear you say, well actually I'll need to insert a value into a space at an index, List<int> doesn't allow you to do that. So moving on.
+//Slow example of a function of N^2 as it uses nested loops, there is a better way to write this using python, and arrays that don't require you to set a size, whereas C# requires you to do this. Use a list I hear you say, well actually I'll need to insert a value into a cell at an index, List<int> doesn't allow you to do that. So moving on.
 bool HasDuplicateValue(int[] array)
 {
     steps = 0;
---
+++ Algorithms and data structures/SelectionSort.cs
@@ -0,0 +1,29 @@
+﻿//Selection Sort
+int[] SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int lowestNumberIndex = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[lowestNumberIndex])
+                lowestNumberIndex = j;
+        }
+        if (lowestNumberIndex != i)
+        {
+            int temp = array[i];
+            array[i] = array[lowestNumberIndex];
+            array[lowestNumberIndex] = temp;
+        }
+    }
+    return array;
+}
+
+int[] sortedArray = SelectionSort(new int[] { 1, 6, 3, 6, 3 });
+string str = "";
+
+for (int i = 0; i < sortedArray.Length; i++)
+{
+    str += sortedArray[i].ToString();
+}
+Console.WriteLine(str);
