8615d6d6b8b39c7d2d96bf736d292fb233ee7310
charlesburt01 <ctf_exe@hotmail.com>
2023-04-03 13:31:59 +0100

    Adding insetion sort

---
+++ Algorithms and data structures/InsertionSort.cs
@@ -0,0 +1,50 @@
+﻿//Insertion Sort
+
+using System;
+
+int[] InsertionSort(int[] array)
+{
+    //start at the second cell in the array, and run forward through each cell, until reaching the end
+    for (int i = 1; i < array.Length; i++)
+    {
+        //cache the current cell's value, from the outer loop
+        //this value increases
+        int i_leadingPositionValue = array[i];
+        //cache the previous cell's value
+        //this value decreases
+        int d_cellIndexBefore = i - 1;
+
+        //this while loop has 2 exit parameters
+        //only run until positionbefore reaches leftmost value
+        //run backwards from the position before
+        while (d_cellIndexBefore >= 0)
+        {
+            //if the value at the position before is greater than the leading position
+            if (array[d_cellIndexBefore] > i_leadingPositionValue)
+            {
+                //shift or move the value forward
+                array[d_cellIndexBefore + 1] = array[d_cellIndexBefore];
+                //then look at the next position before
+                d_cellIndexBefore--;
+            }
+            //if the value at the position before isn't greater than the leading position, jump out of the while loop
+            else
+            {
+                break;
+            }
+        }
+        //by this time we have found the correct position for the leadingPosition, and moved everything forward, so now we insert it in its proper place
+        array[d_cellIndexBefore + 1] = i_leadingPositionValue;
+    }
+    return array;
+}
+
+int[] arr = new int[] { 4, 2, 7, 1, 3 };// 2, 4, 7, 1, 3 // 2, 4, 1, 7, 3 // 2, 1, 4, 7, 3 /// 1, 2, 4, 7, 3 /// 1, 2, 4, 3, 7 ///  1, 2, 4, 3, 7///  1, 2, 3, 4, 7
+arr = InsertionSort(arr);
+string output = "";
+
+for (int i = 0; i < arr.Length; i++)
+{
+    output += arr[i];
+}
+Console.WriteLine(output);
--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -47,3 +47,37 @@ no example yet
 
 */
 
+
+
+
+//Insertion Sort
+
+List<int> intersection(int[] firstArray, int[] secondArray)
+{
+    List<int> arr = new List<int>();
+
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        for (int j = 0; j < secondArray.Length; j++)
+        {
+            if (firstArray[i] == secondArray[j])
+            {
+                arr.Add(firstArray[i]);
+                break;
+            }
+        }
+    }
+
+    return arr;
+}
+
+int[] arr1 = new int[] { 4, 2, 7, 1, 3 };// 2, 4, 7, 1, 3 // 2, 4, 1, 7, 3 // 2, 1, 4, 7, 3 /// 1, 2, 4, 7, 3 /// 1, 2, 4, 3, 7 ///  1, 2, 4, 3, 7///  1, 2, 3, 4, 7
+int[] arr2 = new int[] { 1, 2, 3, 3, 0 };// 2, 4, 7, 1, 3 // 2, 4, 1, 7, 3 // 2, 1, 4, 7, 3 /// 1, 2, 4, 7, 3 /// 1, 2, 4, 3, 7 ///  1, 2, 4, 3, 7///  1, 2, 3, 4, 7
+List<int> arr = intersection(arr1,arr2);
+string output = "";
+
+for (int i = 0; i < arr.Count; i++)
+{
+    output += arr[i];
+}
+Console.WriteLine(output);
