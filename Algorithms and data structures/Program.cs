f4fe480494a588cd238c3024626f15633826d47d
charlesburt01 <ctf_exe@hotmail.com>
2023-04-22 17:22:12 +0100

    Adding queueprintmanager and fixing compilation error with csproj

--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -4,6 +4,6 @@ The property of the other .cs files are build action none, so they dont compile,
 
 
 
-//Queues - a print job manager for a document printer
+
 
 
---
+++ Algorithms and data structures/Queues/QueuePrintManager.cs
@@ -0,0 +1,31 @@
+﻿//Queues - a print job manager for a document printer
+
+using System;
+using System.Collections;
+
+PrintManager printManager = new PrintManager();
+printManager.QueuePrintJob("Doc 1"); // expected = Doc 1
+printManager.QueuePrintJob("Doc 2"); // expected = Doc 2
+printManager.QueuePrintJob("Doc 3"); // expected = Doc 3
+printManager.Run();
+
+class PrintManager
+{
+    Queue printQueue;
+    public PrintManager()
+    {
+        printQueue = new Queue();
+    }
+    public void QueuePrintJob(string document)
+    {
+        printQueue.Enqueue(document);
+    }
+    public void Run()
+    {
+        while (printQueue.Count > 0)
+        {
+            Console.WriteLine(printQueue.Dequeue());
+        }
+    }
+}
+
