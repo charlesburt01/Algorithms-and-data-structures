f0eb1a6fb784f1cc96be6f3064c564cda7d88873
charlesburt01 <ctf_exe@hotmail.com>
2023-04-22 19:48:09 +0100

    Adding Stack Exercise Maximum Nesting Depth of the Parentheses

--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -2,14 +2,3 @@
 The property of the other .cs files are build action none, so they dont compile, they are just files to copy paste from into here
 */
 
-
-
-
-//chapter 9
-/*Q1 = queue
- * Q2 = 4
- * Q3 = 3
- * */
-
-
-
