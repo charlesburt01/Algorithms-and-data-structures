73896b455f0348d960e8415ad6d9473fca274178
charlesburt01 <ctf_exe@hotmail.com>
2023-04-25 16:00:05 +0100

    adding recursive summation

--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -1,4 +1,3 @@
 ﻿/*Use this file to run the code, copy paste the code from the other files
 The property of the other .cs files are build action none, so they dont compile, they are just files to copy paste from into here
 */
-
---
+++ Algorithms and data structures/Recursion/RecursiveSummation.cs
@@ -0,0 +1,7 @@
+﻿//recursive summation of all numbers (inclusive) inbetween low and high bounds
+int sum(int low, int high)
+{
+    if (high == low) return low;
+    return high + sum(low, high - 1);
+}
+Console.WriteLine("Expected = 55, actual = " + sum(1, 10));
