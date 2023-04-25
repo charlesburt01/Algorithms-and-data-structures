59db072534e921d75a0fe0520fdc02622bc18d74
charlesburt01 <ctf_exe@hotmail.com>
2023-04-25 15:12:08 +0100

    Added recursion

--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -2,3 +2,4 @@
 The property of the other .cs files are build action none, so they dont compile, they are just files to copy paste from into here
 */
 
+//Recursion
---
+++ Algorithms and data structures/Recursion/RecursionCountdown.cs
@@ -0,0 +1,19 @@
+﻿//Recursion
+//Old method
+void Countdown(int time)
+{
+    for (int i = time; i >= 0; i--)
+    {
+        Console.WriteLine(i);
+    }
+}
+//Countdown(10);
+
+//Recursive method
+void CountdownRecursive(int number)
+{
+    Console.WriteLine(number);
+    if (number == 0) return;
+    CountdownRecursive(number -= 1);
+}
+CountdownRecursive(10);
