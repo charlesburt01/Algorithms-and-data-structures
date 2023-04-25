e9e536b34c616b713d2506649ae3c7de96e068a7
charlesburt01 <ctf_exe@hotmail.com>
2023-04-25 19:57:47 +0100

    Added recursive factorial(bottom up)

--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -1,3 +1,5 @@
 ﻿/*Use this file to run the code, copy paste the code from the other files
 The property of the other .cs files are build action none, so they dont compile, they are just files to copy paste from into here
 */
+
+
---
+++ Algorithms and data structures/Recursion/RecursionBottomUpFactorial.cs
@@ -0,0 +1,8 @@
+﻿//bottom up recursive factorial (multiply all numbers up to a number, starting from 1)
+int factorial(int n, int i = 1, int product = 1)
+{
+    Console.WriteLine($"n = {n}, i={i}, product={product}");
+    if (i > n) return product;
+    return factorial(n, i + 1, product * i);
+}
+Console.WriteLine(factorial(2));
