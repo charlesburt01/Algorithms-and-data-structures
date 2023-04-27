54370521cf0ec47fd121d806364817345b4c80e1
charlesburt01 <ctf_exe@hotmail.com>
2023-04-27 10:25:22 +0100

    adding another version to string reverse

--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -3,11 +3,3 @@ The property of the other .cs files are build action none, so they dont compile,
 */
 
 
-//Recursive string reversal even simpler
-string reverseEvenSimpler(string forward)
-{
-    if (forward.Length == 1) return forward[0].ToString();
-    return reverseEvenSimpler(forward.Substring(1, forward.Length - 1)) + forward[0];
-}
-Console.WriteLine("Expected = cba, actual = " + reverseEvenSimpler("abc"));
-Console.WriteLine("Expected = gfedcba, actual = " + reverseEvenSimpler("abcdefg"));
--- Algorithms and data structures/Recursion/RecursionStringReversal.cs
+++ Algorithms and data structures/Recursion/RecursionStringReversal.cs
@@ -30,3 +30,13 @@ string reverseEvenSimpler(string forward)
 }
 Console.WriteLine("Expected = cba, actual = " + reverseEvenSimpler("abc"));
 Console.WriteLine("Expected = gfedcba, actual = " + reverseEvenSimpler("abcdefg"));
+
+
+//Recursive string reversal another version
+string reverseAnotherVersion(string forward)
+{
+    if (forward.Length == 1) return forward[0].ToString();
+    return reverseAnotherVersion(forward.Substring(1, forward.Length - 1)) + forward[0];
+}
+Console.WriteLine("Expected = cba, actual = " + reverseAnotherVersion("abc"));
+Console.WriteLine("Expected = gfedcba, actual = " + reverseAnotherVersion("abcdefg"));
