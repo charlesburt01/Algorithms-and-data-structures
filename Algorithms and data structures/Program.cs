2844bf9c601b7bfa9b5391aaac9e376cebfd806e
charlesburt01 <ctf_exe@hotmail.com>
2023-04-27 07:48:31 +0100

    adding recursion arraysum

--- Algorithms and data structures/Program.cs
+++ Algorithms and data structures/Program.cs
@@ -3,17 +3,3 @@ The property of the other .cs files are build action none, so they dont compile,
 */
 
 
-//recursively (top down) add all numbers inside an array and modify it in place
-/*int add(int[] array)
-{
-    int[] reducingArray = new int[array.Length-1];
-    Array.Copy(array, 1, reducingArray, 0, array.Length - 1);
-    if (reducingArray.Length > 0) 
-        return array[0] + add(reducingArray);
-    else
-        return 
-}
-
-Console.WriteLine(add(new int[] { 1,2,3,4,5 }));*/
-
-
---
+++ Algorithms and data structures/Recursion/RecursionArraySum.cs
@@ -0,0 +1,12 @@
+﻿//recursively (top down) add all numbers inside an array
+using System;
+
+int arraySum(int[] array, int sum = 0)
+{
+    sum = array[0] + array[1];
+    if (array.Length <= 2) return sum;
+    int[] shorterArray = new int[array.Length - 2];
+    Array.Copy(array, 2, shorterArray, 0, array.Length - 2);
+    return sum + arraySum(shorterArray);
+}
+Console.WriteLine("Expected = 55, actual = " + arraySum(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }));
