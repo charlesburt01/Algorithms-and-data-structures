be6a0547c4de069d89b1d4dde6cbfab10d342021
charlesburt01 <ctf_exe@hotmail.com>
2023-04-27 07:31:31 +0100

    adding recursiontopdownfactorial

---
+++ Algorithms and data structures/Recursion/RecusrsionTopDownFactorial.cs
@@ -0,0 +1,11 @@
+﻿//Recursion top down factorial
+using System;
+
+int factorial(int number)
+{
+    if (number == 1)
+        return 1;
+    else
+        return number * factorial(number - 1);
+}
+Console.WriteLine("expected = 6, actual = " + factorial(3));
