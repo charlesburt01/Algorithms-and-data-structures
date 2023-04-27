25428cec3ecd753ce48d0b6f22c3503fb78c3fe8
charlesburt01 <ctf_exe@hotmail.com>
2023-04-27 09:09:55 +0100

    adding simpler string reversals

---
+++ Algorithms and data structures/Recursion/RecursionStringReversal.cs
@@ -0,0 +1,32 @@
+﻿//Recursive string reversal
+using System;
+
+string reverse(string forward)
+{
+    if (forward.Length == 1) return forward[0].ToString();
+    string lastLetter = forward[forward.Length - 1].ToString();
+    return lastLetter += reverse(forward.Substring(0, forward.Length - 1));
+}
+Console.WriteLine("Expected = cba, actual = " + reverse("abc"));
+Console.WriteLine("Expected = gfedcba, actual = " + reverse("abcdefg"));
+
+
+
+//Recursive string reversal simpler
+string reverseSimpler(string forward)
+{
+    if (forward.Length == 1) return forward[0].ToString();
+    return forward[forward.Length - 1].ToString() + reverseSimpler(forward.Substring(0, forward.Length - 1));
+}
+Console.WriteLine("Expected = cba, actual = " + reverseSimpler("abc"));
+Console.WriteLine("Expected = gfedcba, actual = " + reverseSimpler("abcdefg"));
+
+
+//Recursive string reversal even simpler
+string reverseEvenSimpler(string forward)
+{
+    if (forward.Length == 1) return forward[0].ToString();
+    return forward[^1].ToString() + reverseEvenSimpler(forward[..^1]);
+}
+Console.WriteLine("Expected = cba, actual = " + reverseEvenSimpler("abc"));
+Console.WriteLine("Expected = gfedcba, actual = " + reverseEvenSimpler("abcdefg"));
