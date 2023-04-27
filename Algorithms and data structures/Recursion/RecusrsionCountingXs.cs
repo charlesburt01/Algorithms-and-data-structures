ee81e68b3d59dd1af1a8e258e0d5efa4d8438e65
charlesburt01 <ctf_exe@hotmail.com>
2023-04-27 11:01:49 +0100

    adding Recursive counting X

---
+++ Algorithms and data structures/Recursion/RecusrsionCountingXs.cs
@@ -0,0 +1,54 @@
+﻿
+//Recursive counting X, counts the number of Xs in a given string
+using System;
+
+int CountX(string s, int x = 0)
+{
+    if (s.Length == 1)
+    {
+        if (s[0].ToString() == "x")
+            return x + 1;
+        else
+            return x;
+    }
+    if (s[0].ToString() == "x")
+        return CountX(s.Substring(1, s.Length - 1), x + 1);
+    else
+        return CountX(s.Substring(1, s.Length - 1), x);
+}
+
+Console.WriteLine("Expected = 3, actual = " + CountX("axbxcxd"));
+Console.WriteLine("Expected = 5, actual = " + CountX("axxbxcxdx"));
+
+
+int CountXSimple(string s)
+{
+    if (s.Length == 1)
+    {
+        if (s[0].ToString() == "x")
+            return 1;
+        else
+            return 0;
+    }
+    if (s[0].ToString() == "x")
+        return 1 + CountX(s.Substring(1, s.Length - 1));
+    else
+        return CountX(s.Substring(1, s.Length - 1));
+}
+
+Console.WriteLine("Expected = 3, actual = " + CountXSimple("axbxcxd"));
+Console.WriteLine("Expected = 5, actual = " + CountXSimple("axxbxcxdx"));
+
+
+int CountXSimpler(string s)
+{
+    if (s.Length == 0)
+        return 0;
+    if (s[0].ToString() == "x")
+        return 1 + CountX(s.Substring(1, s.Length - 1));
+    else
+        return CountX(s.Substring(1, s.Length - 1));
+}
+
+Console.WriteLine("Expected = 3, actual = " + CountXSimpler("axbxcxd"));
+Console.WriteLine("Expected = 5, actual = " + CountXSimpler("axxbxcxdx"));
